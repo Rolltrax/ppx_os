18dce4c84619bb8435a329d7698d7b8635e429a7
Zach Baylin <me@zachbayl.in>
2019-12-23 15:21:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ppx_os
+A basic ppx to inject the operating system as a string at compile time
---
+++ src/ppx_os.ml
@@ -0,0 +1,29 @@
+open Ppxlib
+
+let name = "getenv"
+
+
+let os = match Sys.backend_type with
+  | Other(str) -> str
+  | Native
+  | Bytecode -> match Sys.os_type with
+    | "Win32" -> "Windows"
+    | _ ->
+      let otpt = Unix.open_process_in("uname") in
+      let uname = input_line(otpt) in
+      let _ = close_in(otpt) in
+      match uname with
+        | "Darwin" -> "macOS"
+        | str -> str
+
+let expand ~loc ~path:_ =
+  [%expr [%e Ast_builder.Default.estring os ~loc]]
+
+let ext =
+  Extension.declare
+    name
+    Extension.Context.expression
+    Ast_pattern.(pstr nil)
+    expand
+
+let () = Ppxlib.Driver.register_transformation name ~extensions:[ext]
